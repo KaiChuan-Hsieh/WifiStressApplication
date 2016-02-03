e5f7731f5cea6801fd71eaabdb915e5b26622f93
Kirt Hsieh <kirt_hsieh@asus.com>
2016-02-03 12:07:59 +0800

    FtpFragment update
    1. Add prograss dialog when downloading file
    2. Add Total Rounds and Pause Duration in setting

--- app/src/main/java/com/wifi/wifistressapplication/FtpFragment.java
+++ app/src/main/java/com/wifi/wifistressapplication/FtpFragment.java
@@ -1,8 +1,10 @@
 package com.wifi.wifistressapplication;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.Fragment;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import java.io.File;
@@ -34,6 +37,7 @@ public class FtpFragment extends Fragment {
     private EditText loginPwd;
     private EditText destPath;
     private EditText testRounds;
+    private EditText testDuration;
     private ToggleButton mBtn;
     private long startTime;
     private long endTime;
@@ -41,6 +45,9 @@ public class FtpFragment extends Fragment {
     private boolean testStarted = false;
     private int totalRounds = 0;
     private String ftpUrl;
+    private CountDownTimer mTimer;
+    private TextView mLeftTime;
+    private ProgressDialog mProgressDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -57,7 +64,16 @@ public class FtpFragment extends Fragment {
         loginPwd = (EditText) activity.findViewById(R.id.password);
         destPath = (EditText) activity.findViewById(R.id.file_path);
         testRounds = (EditText) activity.findViewById(R.id.rounds);
+        testDuration = (EditText) activity.findViewById(R.id.duration);
         mBtn = (ToggleButton) activity.findViewById(R.id.button);
+        mLeftTime = (TextView) activity.findViewById(R.id.left_time);
+
+        mProgressDialog = new ProgressDialog(getContext());
+        mProgressDialog.setTitle("Downloading...");
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setProgress(ProgressDialog.STYLE_HORIZONTAL);
+        mProgressDialog.setMax(100);
+        mProgressDialog.setCancelable(false);
 
         mBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -72,9 +88,25 @@ public class FtpFragment extends Fragment {
                     String targetPath = String.valueOf(destPath.getText());
                     ftpUrl = String.format("ftp://%s:%s@%s/%s", user, pwd, dest, targetPath);
                     Log.i(TAG, "URL: " + ftpUrl);
+                    int duration = Integer.parseInt(testDuration.getText().toString());
+                    mTimer = new CountDownTimer(duration*1000, 1000) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            int sec = ((int) millisUntilFinished)/1000;
+                            mLeftTime.setText(String.valueOf(sec));
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            mLeftTime.setText("");
+                            new DownloadFtpTask().execute(ftpUrl);
+                        }
+                    };
                     new DownloadFtpTask().execute(ftpUrl);
                 } else {
                     Log.i(TAG, "Test Stopped");
+                    mLeftTime.setText("");
+                    mTimer.cancel();
                     testStarted = false;
                 }
             }
@@ -101,28 +133,79 @@ public class FtpFragment extends Fragment {
         */
     }
 
-    private class DownloadFtpTask extends AsyncTask<String, Void, Boolean> {
+    private class DownloadFtpTask extends AsyncTask<String, Integer, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog.show();
+            mProgressDialog.setIndeterminate(false);
+        }
         @Override
         protected Boolean doInBackground(String... params) {
             int count = params.length;
-            boolean res = false;
+            boolean res = true;
             try {
                 for (int i = 0; i < count; i++) {
-                    res = downloadUrl(params[i]);
+                    String localPath = "/sdcard/Download/" + getFileName(params[i]);
+                    File file = new File(localPath);
+                    String downPath = localPath;
+                    long fileLength = -1;
+                    if (file.exists()) {
+                        fileLength = file.length();
+                        downPath = localPath + ".test";
+                    }
+                    FileOutputStream fos = new FileOutputStream(downPath);
+                    byte[] buffer = new byte[BUFFER_SIZE];
+                    int byteRead = -1;
+                    URL url = new URL(params[i]);
+                    URLConnection conn = url.openConnection();
+                    InputStream is = conn.getInputStream();
+
+                    long downLength = 0;
+                    startTime = System.currentTimeMillis();
+                    while ((byteRead = is.read(buffer)) != -1) {
+                        downLength += byteRead;
+                        fos.write(buffer, 0, byteRead);
+                        if (fileLength > 0)
+                            publishProgress((int) (downLength*100/fileLength));
+                        else
+                            publishProgress(-1);
+                    }
+                    is.close();
+                    fos.close();
+                    endTime = System.currentTimeMillis();
+
+                    if (!isDownloadSuccessful(localPath, downPath)) {
+                        res = false;
+                        break;
+                    }
                 }
                 return res;
             } catch (IOException e) {
+                mProgressDialog.dismiss();
                 e.printStackTrace();
                 return false;
             }
         }
 
         @Override
+        protected void onProgressUpdate(Integer... progress) {
+            super.onProgressUpdate(progress);
+            if (progress[0] < 0) {
+                mProgressDialog.setMessage("Sample File Downloading...");
+            } else {
+                String res = String.format("%d/100", progress[0]);
+                mProgressDialog.setMessage(res);
+            }
+        }
+
+        @Override
         protected void onPostExecute(Boolean result) {
+            mProgressDialog.dismiss();
             Message msg = new Message();
             if (result) {
                 msg.what = DOWNLOAD_COMPLETE;
-                mHandler.sendMessageDelayed(msg, 10000);
+                mHandler.sendMessage(msg);
             } else {
                 msg.what = DOWNLOAD_FAILED;
                 mHandler.sendMessage(msg);
@@ -138,7 +221,7 @@ public class FtpFragment extends Fragment {
                     Log.i(TAG, "DOWNLOAD_COMPLETE received!");
                     totalRounds--;
                     if (testStarted && totalRounds > 0) {
-                        new DownloadFtpTask().execute(ftpUrl);
+                        mTimer.start();
                     } else {
                         mBtn.setChecked(false);
                     }
@@ -149,33 +232,6 @@ public class FtpFragment extends Fragment {
             }
         }
     };
-
-    private boolean downloadUrl(String myUrl) throws IOException {
-        Log.i(TAG, "downloadUrl in");
-        InputStream is = null;
-        String localPath = "/sdcard/Download/" + getFileName(myUrl);
-        File file = new File(localPath);
-        String downPath = localPath;
-        if (file.exists()) {
-            downPath = localPath + ".test";
-        }
-        FileOutputStream fos = new FileOutputStream(downPath);
-        byte[] buffer = new byte[BUFFER_SIZE];
-        int byteRead = -1;
-        URL url = new URL(myUrl);
-        URLConnection conn = url.openConnection();
-        is = conn.getInputStream();
-        startTime = System.currentTimeMillis();
-        while ((byteRead = is.read(buffer)) != -1) {
-            fos.write(buffer, 0, byteRead);
-        }
-
-        is.close();
-        fos.close();
-
-        endTime = System.currentTimeMillis();
-        return isDownloadSuccessful(localPath, downPath);
-    }
 
     private boolean isDownloadSuccessful(String local, String down) {
         File localFile = new File(local);
